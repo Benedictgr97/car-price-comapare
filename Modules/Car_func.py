a8fb3c467d9d5e162e201dae1addb2d9fa0ef64e
Benedictgr97 <benedictgrant97@gmail.com>
2023-05-22 19:48:08 +0100

    rename file

--- Modules/Car_func.py
+++ Modules/Car_func.py
@@ -1,13 +1,7 @@
-import requests
-import json
-import csv
 from bs4 import BeautifulSoup
 import traceback
 import cloudscraper
 import pandas as pd
-import numpy as np
-import seaborn as sns
-from decimal import Decimal
 from re import sub
 import matplotlib.pyplot as plt
 
