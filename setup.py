0357957489c5fa7c6fd5e52c2b4e0442a3585b8f
Benedict grant <64258751+Benedictgr97@users.noreply.github.com>
2023-05-28 12:31:47 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='tds-recommender',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Recommender algorithm for DTP',
+    author='tools & data science',
+    license='',
+)
